422bc751013a22d9e27fa22fe6fa3a66a5311da0
Azat Faezov <farscince@gmail.com>
2024-08-06 17:34:43 +0500

    language switching

--- components/ui/top-panel.tsx
+++ components/ui/top-panel.tsx
@@ -2,13 +2,16 @@
 
 import Image from 'next/image'
 import { useReactToPrint } from 'react-to-print'
+import { Link, usePathname, useRouter } from '@/navigation'
+import { useParams } from 'next/navigation'
+import { defaultLocale } from '@/config'
 
 const CV_FILE_NAME = 'faezov-cv.pdf'
 
 type Props = {}
 
-const TopPanel = ({ }: Props) => {
-
+const TopPanel = ({}: Props) => {
+  const { locale } = useParams()
   const t = (phrase) => phrase
   const createPrintView = useReactToPrint({
     documentTitle: CV_FILE_NAME,
@@ -19,30 +22,34 @@ const TopPanel = ({ }: Props) => {
 
   return (
     <aside className="sticky top-0 z-40 bg-primary print:hidden">
-      <div className="mx-auto flex max-w-screen-lg items-center justify-end py-1 pr-8 gap-4">
-        <button
-          title={t('Print CV')}
-          onClick={printDocument}
-        >
-          <Image
-            width={20}
-            height={20}
-            src="/assets/icons/print.svg"
-            alt="download"
-            className=""
-          />
+      <div className="mx-auto flex max-w-screen-lg items-center justify-end gap-4 py-1 pr-8">
+        <button title={t('Print CV')} onClick={printDocument}>
+          <Image width={20} height={20} src="/assets/icons/print.svg" alt="download" className="" />
         </button>
-        {/*<button*/}
-        {/*  className='w-5 h-5 underline underline-offset-4 border-white rounded-full shrink-0 flex place-items-center text-xs text-white font-semibold'*/}
-        {/*  title={t('Change Language')}*/}
-        {/*>*/}
-        {/*  Ru*/}
-        {/*</button>*/}
+        {locale === defaultLocale ? (
+          <Link
+            title={t('Change Language')}
+            className="flex h-5 w-5 shrink-0 place-items-center rounded-full border-white text-xs font-semibold text-white underline underline-offset-4"
+            href="/"
+            locale="ru"
+          >
+            EN
+          </Link>
+        ) : (
+          <Link
+            title={t('Change Language')}
+            className="flex h-5 w-5 shrink-0 place-items-center rounded-full border-white text-xs font-semibold text-white underline underline-offset-4"
+            href="/"
+            locale="en"
+          >
+            RU
+          </Link>
+        )}
       </div>
     </aside>
   )
-};
+}
 
-TopPanel.displayName = 'TopPanel'; // Setting the display name for the component
+TopPanel.displayName = 'TopPanel' // Setting the display name for the component
 
 export default TopPanel
